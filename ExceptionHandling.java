b10dd61e5813898e04e25c7d3c14999778e6e38d
kajal <kajal@gmail.com>
2023-08-31 21:12:20 +0530

    ...

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,5 +1,28 @@
 public class ExceptionHandling {
+
+    public static void doSomethingEvenMore(){
+        System.out.println("Line 7 = do Something even more");
+        int a=90/0;
+        System.out.println("Line 8 = doSomething even more");
+    }
+    public static void doSomethingMore(){
+        System.out.println("Line 5 = doSomethingMore");
+        doSomethingEvenMore();
+        System.out.println("Line 6 = doSomethingMore");
+    }
+    public static void  doSomething(){
+        System.out.println("Line 3 = doSomething");
+        doSomethingMore();
+        System.out.println("Line 4 = do something ");
+    }
     public static void main(String[] args) {
+
+        System.out.println("Line 1 = main");
+        doSomething();
+        System.out.println("Line 2 = main");
+
+
+
     //Syntax error = when we compile our program 
     //java has defined its set of rules and regulation that you have to follow 
     //in order for jvm to understand what you are writing
@@ -13,29 +36,31 @@ public class ExceptionHandling {
     //errors cannot be handled
 
     //catch block it handles exception =>when exception occurs    
-    try{
-            int number1 = 45;
-            int number2= 0;
-             //int ans= number1/number2; 
-            String str= null;
-            //int len=str.length();
-            int arr[]=new int[4];
-            arr[5]=90;
+    // try{
+    //         int number1 = 45;
+    //         int number2= 0;
+    //          //int ans= number1/number2; 
+    //         String str= null;
+    //         //int len=str.length();
+    //         int arr[]=new int[4];
+    //         // arr[5]=90;
            
-        }
-        catch(NullPointerException n){
-            System.out.println("null pointer excetion has come");
-        }
-        catch(ArithmeticException e){
-            System.out.println("arithmetic exception is handled");
-        }
-        catch(Exception e){
-            System.out.println("this is exception");
-        }
+    //     }
+    //     catch(NullPointerException n){
+    //         System.out.println("null pointer excetion has come");
+    //     }
+    //     catch(ArithmeticException e){
+    //         System.out.println("arithmetic exception is handled");
+    //     }
+    //     catch(Exception e){
+    //         System.out.println("this is exception");
+    //     }
+    //     finally{
+    //         System.out.println("Now you can give exam");
+    //     } 
             
-            System.out.println("Now you can give exam");
-            System.out.println("now you can happily code");
-            System.out.println("iam in main function");    
+    //         System.out.println("now you can happily code");
+    //         System.out.println("iam in main function");    
     }
     
 }

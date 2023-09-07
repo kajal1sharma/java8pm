4445e18acbf36ef9cfe9c6bed762ecd317f50390
kajal <kajal@gmail.com>
2023-09-07 21:25:53 +0530

    ..

--- MultiThreading1.java
+++ MultiThreading1.java
@@ -1,4 +1,5 @@
-public class MultiThreading1 extends Thread{
+//public class MultiThreading1 extends Thread{
+public class MultiThreading1 implements Runnable{
 
     public void run(){
         for(int i=0;i<100;i++){
@@ -6,8 +7,12 @@ public class MultiThreading1 extends Thread{
         }
     }
     public static void main(String[] args) {
-        MultiThreading1 mt = new MultiThreading1();
-        mt.start();
+        // MultiThreading1 mt = new MultiThreading1();
+        // mt.start();
+
+
+        Thread t= new Thread(new MultiThreading1());
+        t.start();
         for(int i=0;i<100;i++){
             System.out.println("iam main thread");
         }

17e578dc77b1d5a789b33758362b0a77dc94d75d
kajal <kajal@gmail.com>
2023-09-20 20:56:07 +0530

    ..

---
+++ Generics.java
@@ -0,0 +1,76 @@
+
+class Stack2 <E> {
+ 
+    E arr[];
+    int top;
+    Stack2(int size){
+        this.arr= (E[])(new Object[size]);
+        top=-1;
+    }
+
+    boolean push(E data){
+        if(top==arr.length-1){
+            return false;
+        }
+        else{
+            top++;
+            arr[top]=data;
+            return true;
+        }
+    }
+
+    E pop(){
+        if(top==-1){
+            
+            return null;
+        }
+        else{
+            E ele = this.arr[top];
+            top--;
+            return ele;
+        }
+    }
+
+}
+
+class Car{
+int a;
+Car(int a){
+    this.a=a;
+}
+}
+
+public class Generics {
+    public static void main(String[] args) {
+        // Stack2<Integer> st = new Stack2<Integer>(5);
+        // st.push(23);
+        // st.push(34);
+        // System.out.println(st.pop());
+        Car c= new Car(23);
+        Car c2= new Car(60);
+
+        Stack2<Car> st2 = new Stack2<Car>(5);
+        st2.push(c);
+        st2.push(c2);
+        System.out.println(st2.pop().a);
+        // Stack2<Character> st3 = new Stack2<Character>(5);
+            // Stack2 st = new Stack2(5);
+            // st.push(23);
+            // st.push(24);
+            // st.pop();
+        }    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

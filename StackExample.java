28943e4b12a01de734bdb45e10945576081f35b0
kajal <kajal@gmail.com>
2023-09-26 20:34:49 +0530

    ..

---
+++ StackExample.java
@@ -0,0 +1,7 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+    }
+}
---
+++ WrapperEx.java
@@ -0,0 +1,46 @@
+public class WrapperEx {
+    public static void main(String[] args) {
+        
+        //primitives=> short, byte int long float double char boolean
+        // int a=10;
+        // //properties and function
+
+        // Integer i = new Integer(45);
+        // Integer i2 =90;
+        
+
+
+        // String str="tree";
+        // Integer i= 34;
+        // Byte b= 34;
+
+        // Short s= 54;
+        // Long l = 342l;
+
+
+
+        // Float f = 34.34f;
+        // Double d= 343.3243;
+        // Boolean bool = false;
+        // Character ch= 'a';
+        // Float f2=34.34f;
+        // short s =f.shortValue();
+        // byte b=f.byteValue();
+        // boolean bool= f.equals(f2);
+        
+
+        // Integer obj = 8263782;
+        // String s=Integer.toString(obj);
+       
+        // char ch[] =s.toCharArray();
+        // for(int i=0;i<ch.length/2;i++){
+        //     char temp = ch[i];
+        //     ch[i]=ch[ch.length-i-1];
+        //     ch[ch.length-i-1]=temp;
+
+        // }
+         
+        // String s2=new String(ch);
+        // System.out.println(s2);
+    }
+}

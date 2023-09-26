81734cb85738adec6a7b298cacc40abcdb60c972
kajal <kajal@gmail.com>
2023-09-26 20:56:51 +0530

    ..

--- StackExample.java
+++ StackExample.java
@@ -1,7 +1,13 @@
+import java.util.Collection;
 import java.util.Stack;
 
 public class StackExample {
     public static void main(String[] args) {
-        Stack<Integer> st = new Stack<>();
+        // Stack<Integer> st = new Stack<>();
+        // st.push(34);
+        //collection => storing same type of reference elements
+        Collection<Integer> c=  new Stack<>();
+        //PArent ref = store the ref of child object
+
     }
 }

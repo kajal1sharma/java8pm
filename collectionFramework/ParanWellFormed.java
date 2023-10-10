300ebb9bfb7deb46f3aa5bad6b00d1fef4a10c35
kajal <kajal@gmail.com>
2023-10-10 20:18:35 +0530

    ..

--- collectionFramework/ParanWellFormed.java
+++ collectionFramework/ParanWellFormed.java
@@ -3,25 +3,57 @@ import java.util.Stack;
 public class ParanWellFormed {
     public static void main(String[] args) {
         
-        String str="{{}{}{{}{}}";
+        String str2="{{}{}{{}{}}";
         //Ques => solve for valid parantheses
-        String str2="[({}{[]})]()";
-        String str3="[({}{[]})](){{}()";
+        String str3="[({}{[]})]()";
+        String str="[({}{[]})](){{}()";
        Stack<Character> st= new Stack<>();
 
        boolean isWellFormed = true;
        for(int i=0;i<str.length();i++){
-        if(str.charAt(i)=='{'){
+        if(str.charAt(i)=='{'|| str.charAt(i)=='(' || str.charAt(i)=='['){
             st.push(str.charAt(i));
         }
-        else if( str.charAt(i)=='}'){
+        else{
+            //else if( str.charAt(i)=='}'){
             if(st.size()==0){
                 isWellFormed=false;
                 break;
             }
             else if(st.size()>=1){
-                st.pop();
-                continue;
+                if(str.charAt(i)=='}' ){
+                    char top = st.peek();
+                    if(top=='{'){
+                        st.pop();
+                        continue;
+                    }
+                    else{
+                         isWellFormed=false;
+                         break;
+                    }
+                }
+                else if(str.charAt(i)==']' ){
+                    char top = st.peek();
+                    if(top=='['){
+                        st.pop();
+                        continue;
+                    }
+                    else{
+                         isWellFormed=false;
+                         break;
+                    }
+                }
+                else if(str.charAt(i)==')' ){
+                    char top = st.peek();
+                    if(top=='('){
+                        st.pop();
+                        continue;
+                    }
+                    else{
+                         isWellFormed=false;
+                         break;
+                    }
+                }
             }
         }
        }
